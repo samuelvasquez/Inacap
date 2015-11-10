edc349f3bcc80bc3c103ff799ef8ca7cae881b8e
samuelvasquez <samuelvasquez@gmail.com>
2015-11-09 22:53:49 -0300

    2.1. Agrega archivos relacionado al mapa

---
+++ app/src/main/java/samuelvasquez/inacap/cl/unidad3/MapFragment.java
@@ -0,0 +1,144 @@
+package samuelvasquez.inacap.cl.unidad3;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+import samuelvasquez.inacap.cl.unidad3.dataaccess.DAOCliente;
+import samuelvasquez.inacap.cl.unidad3.datamodel.Cliente;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MapFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapFragment extends Fragment {
+    public static final String ARG_ITEM_ID = "map";
+
+    public static final String ARG_VENDEDOR_ID = "id_vendedor";
+    Activity activity;
+    DAOCliente clienteDAO;
+    MapView mMapView;
+    private int id_vendedor;
+    private GoogleMap googleMap;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ClientesListFragment.
+     */
+    public static MapFragment newInstance(int id_vendedor) {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_VENDEDOR_ID, String.valueOf(id_vendedor));
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+        clienteDAO = new DAOCliente(activity);
+        if (getArguments() != null) {
+            id_vendedor = getArguments().getInt(ARG_VENDEDOR_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // inflate and return the layout
+        View v = inflater.inflate(R.layout.fragment_map, container,
+                false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+
+        // latitude and longitude por defecto
+        double latitude = -33.4691199;
+        double longitude = -70.641997;
+        LatLng posicionInicial = new LatLng(latitude, longitude);
+
+        ArrayList<Cliente> lista = clienteDAO.getClientesPendientesByIdVendedor(id_vendedor);
+        if (lista.size() == 0) {
+            Toast.makeText(activity, "No hay clientes con pedidos pendientes",
+                    Toast.LENGTH_LONG).show();
+
+        } else {
+            for (Cliente item : lista) {
+                LatLng posicion = new LatLng(item.latitud, item.longuitud);
+
+                // create marker
+                MarkerOptions marker = new MarkerOptions().position(
+                        posicion).title(item.nombre);
+
+                // adding marker
+                googleMap.addMarker(marker);
+            }
+        }
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(posicionInicial).zoom(10).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+}
